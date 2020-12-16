6ac5a686b1bc7a7d3212106a094d377cc2333069
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-12-16 11:39:28 +0700

    Add files via upload

--- bot_auto.py
+++ bot_auto.py
@@ -1,10 +1,9 @@
 import discord
-import asyncio
 from discord.ext import commands
 from langdetect import DetectorFactory
 DetectorFactory.seed = 0
 import langdetect as ld
-from ENTH import en2th_text, enth_main
+from ENTH import enth_main
 client = commands.Bot(command_prefix='-') #กำหนด Prefix
 
 @client.event
@@ -16,16 +15,22 @@ async def on_message(message) : #ดักรอข้อความใน Chat
     if message.author == client.user:
         return
     msg = message.content
-    if msg[0:2] == '-t':
-        tmessage = enth_main(msg[2:])
-
-        await message.channel.send(tmessage)
-    elif msg == 'Hello' or msg == 'hello':
-        await message.channel.send("สวัสดีครับท่านสมาชิกชมรมคนชอบ PSIT")
-    elif msg == 'Help' or msg == 'help':
-        await message.channel.send("Use -t 'your text' to convert into another language.(EN/TH)")
+    if msg == '-Help' or msg == '-help' or msg == "-HELP":
+        await message.channel.send("If you type something you didn't mean to, i'll correct it.")
+    if msg[0] == '@' or msg.isnumeric() == True or msg.isspace() == True or msg.isdecimal() == True:
+        return
+    if 'https://www.' in msg or 'www.' in msg:
+        return
+    if msg in open('thai_dict.txt', encoding="utf8").read():
+        return
+    if msg.upper() in open('dictionary.txt').read():
+        return
+    if msg.isascii() == True or ld.detect(msg) == 'th':
+        tmessage = enth_main(msg)
+        mention = str(message.author.mention)
+        await message.channel.send(mention+' said "' + tmessage + '".')
     else:
-         pass
+        return
 
 TOKEN = 'NzgxMzg0NjMwNTE5NTk1MDY4.X783JQ.ArHDvVzyN7vjiqUE3ehEo2Y2aeA'
 client.run(TOKEN) #รันบอท (โดยนำ TOKEN จากบอทที่เราสร้างไว้นำมาวาง)
